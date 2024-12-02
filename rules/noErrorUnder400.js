1cf76cc74e9d5958d5f84629a91bdc002660d647
Jake Downie <jake@bland.ai>
2024-12-01 16:20:10 -0800

    allow null errors

--- rules/noErrorUnder400.js
+++ rules/noErrorUnder400.js
@@ -1,31 +1,45 @@
 module.exports = {
-    meta: {
-        type: "problem",
-        docs: {
-            description: "Ensure responseHandler does not define errors if status is under 400",
-            category: "Best Practices",
-            recommended: false,
-        },
-        schema: [], // no options
+  meta: {
+    type: "problem",
+    docs: {
+      description:
+        "Ensure responseHandler does not define errors if status is under 400",
+      category: "Best Practices",
+      recommended: false,
     },
-    create: function (context) {
-        return {
-            CallExpression(node) {
-                if (node.callee && node.callee.name === 'responseHandler') {
-                    const responseHandlerArgs = node.arguments[0];
-                    if (responseHandlerArgs && responseHandlerArgs.type === 'ObjectExpression') {
-                        const statusProperty = responseHandlerArgs.properties.find(prop => prop.key.name === 'status');
-                        const errorsProperty = responseHandlerArgs.properties.find(prop => prop.key.name === 'errors');
+    schema: [], // no options
+  },
+  create: function (context) {
+    return {
+      CallExpression(node) {
+        if (node.callee && node.callee.name === "responseHandler") {
+          const responseHandlerArgs = node.arguments[0];
+          if (
+            responseHandlerArgs &&
+            responseHandlerArgs.type === "ObjectExpression"
+          ) {
+            const statusProperty = responseHandlerArgs.properties.find(
+              (prop) => prop.key.name === "status"
+            );
+            const errorsProperty = responseHandlerArgs.properties.find(
+              (prop) => prop.key.name === "errors"
+            );
 
-                        if (statusProperty && statusProperty.value.value < 400 && errorsProperty) {
-                            context.report({
-                                node,
-                                message: "Errors should not be defined when status is under 400.",
-                            });
-                        }
-                    }
-                }
+            if (
+              statusProperty &&
+              statusProperty.value.value < 400 &&
+              errorsProperty &&
+              errorsProperty.value !== null
+            ) {
+              context.report({
+                node,
+                message:
+                  "Errors should not be defined when status is under 400.",
+              });
             }
-        };
-    }
+          }
+        }
+      },
+    };
+  },
 };
