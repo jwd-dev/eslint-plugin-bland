e4cbe8ac4a4d888d8e2415056985eac0c7a2df78
Jake Downie <jake@bland.ai>
2024-12-01 16:13:58 -0800

    no-error-under-400

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
     rules: {
         "response-handler-error-source": require('./rules/responseHandlerErrorSource'),
-        "no-generic-try-catch": require('./rules/noGenericTryCatch')
+        "no-generic-try-catch": require('./rules/noGenericTryCatch'),
+        "no-error-under-400": require('./rules/noErrorUnder400'),
     }
 };
---
+++ rules/tests/noErrorUnder400.test.js
@@ -0,0 +1,82 @@
+const { RuleTester } = require("eslint");
+const noErrorUnder400 = require("../noErrorUnder400");
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+});
+ruleTester.run("no-generic-try-catch", noErrorUnder400, {
+  valid: [
+    {
+      code: `
+      instance.post("/fake", async (req, res) => {
+        return responseHandler({
+			res: res,
+			status: 400,
+			data: {
+				status: "success",
+				data: {
+					message: "This is a fake endpoint",
+				},
+			}		});
+});`,
+    },
+	{
+		code: `
+		instance.post("/fake", async (req, res) => {
+		  return responseHandler({
+			  res: res,
+			  status: 200,
+			  data: {
+				  status: "success",
+				  data: {
+					  message: "This is a fake endpoint",
+				  },
+			  }		});
+  });`,
+	  },
+	  {
+		code: `
+		instance.post("/fake", async (req, res) => {
+		  return responseHandler({
+			  res: res,
+			  status: 500,
+			  data: {
+				  status: "success",
+				  data: {
+					  message: "This is a fake endpoint",
+				  },
+			  }		});
+  });`,
+	  },
+  ],
+  invalid: [
+    {
+      code: `
+		instance.post("/fake", async (req, res) => {
+		  return responseHandler({
+			  res: res,
+			  status: 200,
+			  data: {
+				  status: "success",
+				  data: {
+					  message: "This is a fake endpoint",
+				  },
+},
+				  errors: [
+				  {
+				  	error: "UNKNOWN_ERROR",
+					message: "An unknown error occurred",
+				  }]
+			  		});
+  });`,
+      errors: [
+        {
+          message: "Errors should not be defined when status is under 400.",
+        },
+      ],
+    },
+  ],
+});
